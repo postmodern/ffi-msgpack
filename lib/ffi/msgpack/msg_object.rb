8793d2eb197df4af9391377cdc018994710ded18
Postmodern <postmodern.mod3@gmail.com>
2010-05-17 21:31:33 -0700

    Filled out MsgObject.

--- lib/ffi/msgpack/msg_object.rb
+++ lib/ffi/msgpack/msg_object.rb
@@ -1,8 +1,32 @@
-require 'ffi'
+require 'ffi/msgpack/types'
+require 'ffi/msgpack/msg_object_union'
 
 module FFI
   module MsgPack
     class MsgObject < FFI::Struct
+
+      layout :type, :msgpack_object_type,
+             :values, MsgObjectUnion
+
+      #
+      # The type of the Msg Object.
+      #
+      # @return [Symbol]
+      #   The type of the Msg Object.
+      #
+      def type
+        self[:type]
+      end
+
+      #
+      # The values of the Msg Object.
+      #
+      # @return [MsgObjectUnion]
+      #   The values of the Msg Object.
+      #
+      def values
+        self[:values]
+      end
 
     end
   end
