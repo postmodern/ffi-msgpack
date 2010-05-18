c4268ba3f92cd77b31e2ba6402c996d5f6bf9d80
Postmodern <postmodern.mod3@gmail.com>
2010-05-17 21:06:25 -0700

    Added MsgKeyValue.

---
+++ lib/ffi/msgpack/msg_key_value.rb
@@ -0,0 +1,33 @@
+require 'ffi/msgpack/types'
+require 'ffi/msgpack/object'
+
+module FFI
+  module MsgPack
+    class MsgKeyValue < FFI::Struct
+
+      layout :key, :pointer,
+             :value, :pointer
+
+      #
+      # The key.
+      #
+      # @return [MsgObject]
+      #   The key Msg Object.
+      #
+      def key
+        MsgObject.new(self[:key])
+      end
+
+      #
+      # The value.
+      #
+      # @return [MsgObject]
+      #   The value Msg Object.
+      #
+      def value
+        MsgObject.new(self[:value])
+      end
+
+    end
+  end
+end
