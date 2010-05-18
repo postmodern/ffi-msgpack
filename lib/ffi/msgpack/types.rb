499106b0d9da0bb93ff9636e692c2845886cdf9a
Postmodern <postmodern.mod3@gmail.com>
2010-05-17 20:25:34 -0700

    Added msgpack_object_type.

--- lib/ffi/msgpack/types.rb
+++ lib/ffi/msgpack/types.rb
@@ -15,5 +15,16 @@ module FFI
 
     callback :msgpack_packer_write, [:pointer, :pointer, :uint], :int
 
+    enum :msgpack_object_type, [
+      :msgpack_object_nil,              0x00,
+      :msgpack_object_boolean,          0x01,
+      :msgpack_object_positive_integer, 0x02,
+      :msgpack_object_negative_integer, 0x03,
+      :msgpack_object_double,           0x04,
+      :msgpack_object_raw,              0x05,
+      :msgpack_object_array,            0x06,
+      :msgpack_object_map,              0x07
+    ]
+
   end
 end
