cf6a89e0cb034d335d991b70d650e056820db5c5
Postmodern <postmodern.mod3@gmail.com>
2010-05-17 20:54:31 -0700

    Added MsgArray#[].

--- lib/ffi/msgpack/msg_array.rb
+++ lib/ffi/msgpack/msg_array.rb
@@ -1,4 +1,5 @@
 require 'ffi/msgpack/types'
+require 'ffi/msgpack/msg_object'
 
 module FFI
   module MsgPack
@@ -15,6 +16,32 @@ module FFI
       #
       def length
         self[:size]
+      end
+
+      #
+      # The Msg Object at the given index or a field within the Msg Array
+      # Struct.
+      #
+      # @param [Symbol, Integer] key
+      #   The Msg Array Struct field or index within the Array.
+      #
+      # @return [Object, MsgObject]
+      #   The field within the Msg Array Struct or the Msg Object within
+      #   the Array.
+      #
+      # @raise [ArgumentError]
+      #   The given Array index is out of bounds.
+      #
+      def [](key)
+        if key.kind_of?(Integer)
+          if (key >= 0 && key < self[:size])
+            MsgObject.new(self[:ptr].get_pointer(key))
+          else
+            raise(ArgumentError,"invalid array index: #{key}",calller)
+          end
+        else
+          super(key)
+        end
       end
 
     end
