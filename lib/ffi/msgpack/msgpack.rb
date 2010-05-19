08b432ed48f426fa468e8f39f8d1c58d8bbc23bc
postmodern <postmodern.mod3@gmail.com>
2010-05-18 20:28:11 -0700

    Attached msgpack_packer_* functions.

--- lib/ffi/msgpack/msgpack.rb
+++ lib/ffi/msgpack/msgpack.rb
@@ -40,5 +40,47 @@ module FFI
 
     attach_function :msgpack_unpacker_expand_buffer, [:pointer, :size_t], :bool
 
+    callback :msgpack_packer_write, [:pointer, :pointer, :uint], :int
+
+    #attach_function :msgpack_packer_init, [:pointer, :pointer, :msgpack_packer_write], :void
+
+    #attach_function :msgpack_packer_new, [:pointer, :msgpack_packer_write], :pointer
+    #attach_function :msgpack_packer_free, [:pointer], :void
+
+    #attach_function :msgpack_pack_short, [:pointer, :short], :int
+    #attach_function :msgpack_pack_int, [:pointer, :int], :int
+    #attach_function :msgpack_pack_long, [:pointer, :long], :int
+    #attach_function :msgpack_pack_long_long, [:pointer, :int64], :int
+    #attach_function :msgpack_pack_unsigned_short, [:pointer, :ushort], :int
+    #attach_function :msgpack_pack_unsigned_int, [:pointer, :uint], :int
+    #attach_function :msgpack_pack_unsigned_long, [:pointer, :ulong], :int
+    #attach_function :msgpack_pack_unsigned_long_long, [:pointer, :uint64], :int
+
+    #attach_function :msgpack_pack_uint8, [:pointer, :uint8], :int
+    #attach_function :msgpack_pack_uint16, [:pointer, :uint16], :int
+    #attach_function :msgpack_pack_uint32, [:pointer, :uint32], :int
+    #attach_function :msgpack_pack_uint64, [:pointer, :uint64], :int
+    #attach_function :msgpack_pack_int8, [:pointer, :int8], :int
+    #attach_function :msgpack_pack_int16, [:pointer, :int16], :int
+    #attach_function :msgpack_pack_int32, [:pointer, :int32], :int
+    #attach_function :msgpack_pack_int64, [:pointer, :int64], :int
+
+    #attach_function :msgpack_pack_float, [:pointer, :float], :int
+    #attach_function :msgpack_pack_double, [:pointer, :double], :int
+
+    #attach_function :msgpack_pack_nil, [:pointer], :int
+    #attach_function :msgpack_pack_true, [:pointer], :int
+    #attach_function :msgpack_pack_false, [:pointer], :int
+
+    #attach_function :msgpack_pack_array, [:pointer, :uint], :int
+
+    #attach_function :msgpack_pack_map, [:pointer, :uint], :int
+
+    #attach_function :msgpack_pack_raw, [:pointer, :size_t], :int
+
+    #attach_function :msgpack_pack_raw_body, [:pointer, :pointer, :size_t], :int
+
+    attach_function :msgpack_pack_object, [:pointer, Object], :int
+
   end
 end
