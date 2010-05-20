65cefc3f1a18c2aec7dab222874787d79938bb48
postmodern <postmodern.mod3@gmail.com>
2010-05-19 23:21:42 -0700

    Recreated unpacker functions inlined in msgpack/unpacker.h.

--- lib/ffi/msgpack/unpacker.rb
+++ lib/ffi/msgpack/unpacker.rb
@@ -6,13 +6,44 @@ module FFI
     class Unpacker < FFI::Struct
 
       layout :buffer, :pointer,
-             :user, :size_t,
+             :used, :size_t,
              :free, :size_t,
              :off, :size_t,
              :parsed, :size_t,
              :z, :pointer,
              :initial_buffer_size, :size_t,
              :ctx, :pointer
+
+      def reverse_buffer(size)
+        if self[:free] >= size
+          true
+        else
+          MsgPack.msgpack_unpacker_expand_buffer(self,size)
+        end
+      end
+
+      def buffer
+        self[:buffer] + self[:used]
+      end
+
+      def buffer_capacity
+        self[:free]
+      end
+
+      def buffer_consumed!(size)
+        self[:used] += size
+        self[:free] -= size
+
+        return nil
+      end
+
+      def message_size
+        self[:parsed] - self[:off] + self[:used]
+      end
+
+      def parsed_size
+        self[:parsed]
+      end
 
     end
   end
