3087b6fd23e35e8e144ac54bd50b1c4aab1ec9fa
postmodern <postmodern.mod3@gmail.com>
2010-05-20 06:33:45 -0700

    Organizing.

--- spec/unpacker_spec.rb
+++ spec/unpacker_spec.rb
@@ -6,11 +6,6 @@ require 'stringio'
 describe MsgPack::Unpacker do
   before(:all) do
     @packed = "\x01\x02\x03"
-    @expected = [
-      [:positive_integer, 1],
-      [:positive_integer, 2],
-      [:positive_integer, 3]
-    ]
   end
 
   before(:each) do
@@ -38,12 +33,17 @@ describe MsgPack::Unpacker do
 
   it "should unpack each Msg Object from the buffer" do
     objs = []
+    expected = [
+      [:positive_integer, 1],
+      [:positive_integer, 2],
+      [:positive_integer, 3]
+    ]
 
     @unpacker << @packed
     @unpacker.each_object do |obj|
       objs << [obj.type, obj.values[:u64]]
     end
 
-    objs.should == @expected
+    objs.should == expected
   end
 end
