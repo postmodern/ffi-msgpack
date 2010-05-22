976a30e149a238077f039e24849fc6fe54bcf01e
postmodern <postmodern.mod3@gmail.com>
2010-05-21 21:10:02 -0700

    Added documentation to Unpacker.

--- lib/ffi/msgpack/unpacker.rb
+++ lib/ffi/msgpack/unpacker.rb
@@ -26,6 +26,9 @@ module FFI
              :initial_buffer_size, :size_t,
              :ctx, :pointer
 
+      #
+      # Initializes a new unpacker object.
+      #
       def initialize(*arguments)
         super(*arguments)
 
@@ -33,14 +36,38 @@ module FFI
         @stream = nil
       end
 
+      #
+      # Creates a new unpacker object.
+      #
+      # @param [Integer] size
+      #   The buffer size of the unpacker.
+      #
+      # @return [Unpacker]
+      #   The new unpacker.
+      #
       def Unpacker.create(size)
         Unpacker.new(MsgPack.msgpack_unpacker_new(size))
       end
 
+      #
+      # Destroys a previously allocated unpacker object.
+      #
+      # @param [FFI::Pointer] ptr
+      #   The pointer to the allocated unpacker.
+      #
       def Unpacker.release(ptr)
         MsgPack.msgpack_unpacker_free(ptr)
       end
 
+      #
+      # Writes packed data into the buffer of the unpacker.
+      #
+      # @param [String] packed
+      #   The packed data.
+      #
+      # @return [Unpacker]
+      #   The unpacker.
+      #
       def <<(packed)
         # make sure we have space in the buffer
         reserve_buffer(packed.length)
@@ -53,6 +80,16 @@ module FFI
         return self
       end
 
+      #
+      # Reads packed data from a stream into the buffer of the unpacker.
+      #
+      # @param [IO] io
+      #   The stream to read the packed data from.
+      #
+      # @return [Boolean]
+      #   Specifies whether data was read from the stream, or if the stream
+      #   is empty.
+      #
       def read(io)
         reserve_buffer(@chunk_size)
         result = io.read(buffer_capacity)
@@ -65,6 +102,21 @@ module FFI
         end
       end
 
+      #
+      # Enumerates over each Msg Object from the buffer in the unpacker.
+      #
+      # If {#stream} is set, packed data will be read from it, when the
+      # buffer of the unpacker is fully unpacked.
+      #
+      # @yield [obj]
+      #   The given block will be passed each Msg Object.
+      #
+      # @yieldparam [MsgObject] obj
+      #   An unpacked Msg Object.
+      #
+      # @return [Unpacker]
+      #   The unpacker.
+      #
       def each_object
         loop do
           ret = MsgPack.msgpack_unpacker_execute(self)
@@ -93,6 +145,19 @@ module FFI
         return self
       end
 
+      #
+      # Enumerates over each Msg Object from the buffer in the unpacker.
+      #
+      # @yield [obj]
+      #   The given block will be passed each unpacked Ruby Object, from
+      #   the buffer of the unpacker.
+      #
+      # @yieldparam [nil, true, false, Integer, Float, String, Array, Hash]  obj
+      #   A Ruby Object unpacked from the buffer of the unpacker.
+      #
+      # @return [Enumerator, Unpacker]
+      #   If no block is given, an enumerator will be returned.
+      #
       def each
         return enum_for(:each) unless block_given?
 
@@ -103,6 +168,15 @@ module FFI
 
       protected
 
+      #
+      # Reserves space in the buffer.
+      #
+      # @param [Integer] size
+      #   The number of bytes to reserve.
+      #
+      # @return [Boolean]
+      #   Specifies whether the size has been successfully reserved.
+      #
       def reserve_buffer(size)
         if self[:free] >= size
           true
@@ -111,14 +185,34 @@ module FFI
         end
       end
 
+      #
+      # The offset to empty space in the buffer.
+      #
+      # @return [Integer]
+      #   The number of bytes within the buffer.
+      #
       def buffer_offset
         self[:used]
       end
 
+      #
+      # The remaining space of the buffer.
+      #
+      # @return [Integer]
+      #   The number of bytes free in the buffer.
+      #
       def buffer_capacity
         self[:free]
       end
 
+      #
+      # Consums space in the buffer.
+      #
+      # @param [Integer] size
+      #   The number of bytes to be consumed.
+      #
+      # @return [nil]
+      #
       def buffer_consumed!(size)
         self[:used] += size
         self[:free] -= size
@@ -126,10 +220,22 @@ module FFI
         return nil
       end
 
+      #
+      # The size of the unparsed message in the buffer.
+      #
+      # @return [Integer]
+      #   The number of bytes that are unparsed.
+      #
       def message_size
         self[:parsed] - self[:off] + self[:used]
       end
 
+      #
+      # The number of bytes that have been parsed in the buffer.
+      #
+      # @return [Integer]
+      #   The number of parsed bytes.
+      #
       def parsed_size
         self[:parsed]
       end
